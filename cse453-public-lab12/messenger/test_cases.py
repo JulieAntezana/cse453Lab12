dd74c7f111019a6a7b5e4a61d72d7c688436353d
Murffkins <perkins_n@hotmail.com>
2023-11-30 15:07:28 -0600

    added threat list for test cases

---
+++ cse453-public-lab12/messenger/test_cases.py
@@ -0,0 +1,33 @@
+'''
+All messages are accessible to all users. Any user can view any message  
+
+All messages are accessible to all users. Any user can add any message  
+
+All messages are accessible to all users. Any user can modify any message  
+
+All messages are accessible to all users. Any user can delete any message  
+
+Users of any security level are able to view all messages, even ones higher than their security clearance should allow (ie. Public,  Confidential, Privileged, Secret) 
+
+Attacker can get access to users and passwords in the program  
+
+Usernames are printed out when the program first starts  
+
+Change FILE_NAME to upload a different messages file  
+
+Alter messages.txt  
+
+Alter list of usernames/passwords. This could include adding attacker as a user or denying access to legitimate users.  
+
+Access any messages in system (there is no control currently working in the program to prevent anyone from logging in and viewing, modifying, deleting messages)  
+
+Messages do not save who or when the message was edited  
+
+There is no way to add or remove users. Public users are more like guest users.  
+
+The program session does not expire after a set period of disuse  
+
+ 
+
+All test cases should have an input, a name or scenario for the testcase, and an expected output. Above are example threats we gathered from the Week 09 Lab. 
+'''
