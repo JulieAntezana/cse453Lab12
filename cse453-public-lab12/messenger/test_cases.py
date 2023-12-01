43dd3507701aa1648d2f188ceaab99f25cd9242c
Julie Antezana <jantezana.edu@gmail.com>
2023-11-30 19:54:20 -0600

    added test driver

--- cse453-public-lab12/messenger/test_cases.py
+++ cse453-public-lab12/messenger/test_cases.py
@@ -31,3 +31,82 @@ The program session does not expire after a set period of disuse
 
 All test cases should have an input, a name or scenario for the testcase, and an expected output. Above are example threats we gathered from the Week 09 Lab. 
 '''
+import main
+import interact
+import messages
+import message
+import control
+
+# Similar to the message program, this test set does not check access control, yet.
+def test_login() -> dict:
+    return {
+        "name": "Login with valid username and password",
+        "cases": {
+            "AdmiralAbe": "password",
+            "CaptainCharlie": "password",
+            "SeamanSam": "password",
+            "SeamanSue": "password",
+            "SeamanSly": "password",
+            "Murffkins": "password"
+        },
+        "expected": "Welcome, {username}. Please select an option: "
+    }
+
+# def test_view_messages() -> dict:
+#     return {
+#         "name": "Valid user can view list of message properties",
+#         "cases": {
+#             "AdmiralAbe": "Password",
+#             "CaptainCharlie": "password",
+#             "SeamanSam": "password",
+#             "SeamanSue": "password",
+#             "SeamanSly": "password",
+#             "Murffkins": "password"
+#         },
+#         "expected": message.display_properties(message)
+#     }
+
+def login_test_generator(username, password):
+    interact_ = interact.Interact(username, password, messages)
+    return interact_ # return the Interact object
+
+def run_tests(test_set: dict):
+    print("\n{}\n".format(test_set.get("name")))
+    cases = test_set.get('cases')
+    for username, password in cases.items():
+        expected = test_set["expected"].format(username=username)
+        actual = login_test_generator(username, password)
+        print("Expected:", expected)
+        print("Actual:", f"Welcome, {actual._username}. Please select an option:\n")
+        print("===================")
+        
+def driver_program():
+    print_menu()
+
+    while True:
+        choice = input("Please make a choice: ")
+        if choice == "1":
+            print("\nRun Access Control sets of tests")
+            run_tests(test_login())
+            # run_tests(test_tautology())
+            # run_tests(test_union())
+            # run_tests(test_additional_statement())
+            # run_tests(test_comment())
+            continue
+        if choice == "h":
+            print_menu()
+            continue
+        if choice == "q":
+            break
+        print("Invalid choice. Please chose out of available.")
+
+
+def print_menu():
+    print("""
+    1. Set of tests\n
+    h. Display menu\n
+    q. Exit\n""")
+
+
+if __name__ == "__main__":
+    driver_program()
