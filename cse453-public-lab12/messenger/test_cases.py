8ded8090fd3bbe9527955d002ee549276282eedd
Acloudreeves <ree21020@byui.edu>
2023-12-02 15:47:49 -0700

    Austins test cases draft

--- cse453-public-lab12/messenger/test_cases.py
+++ cse453-public-lab12/messenger/test_cases.py
@@ -66,6 +66,76 @@ def test_login() -> dict:
     }
 
 
+def test_update_delete_messages() -> dict: # Austin
+    return {
+        "name": "Interact with Messages",
+        "cases": {
+            "SeamanSly": "password",
+            "Murffkins": "password"
+        },
+        "expected": [
+            {
+                "login_result": "Welcome, SeamanSly. Please select an option:",
+                "update_messages": [
+                    ("1", "Invalid message ID. Please enter a valid ID."),
+                    ("2", "Invalid message ID. Please enter a valid ID."),
+                    ("3", "Invalid message ID. Please enter a valid ID."),
+                    ("100", "Invalid message ID. Please enter a valid ID."),
+                    ("1", "Failed to update message. Message ID '100' does not exist."),
+                    ("2", "Failed to update message. Message ID '100' does not exist."),
+                    ("3", "Failed to update message. Message ID '100' does not exist."),
+                    ("1", "Successfully updated message. Message ID '1'."),
+                    ("2", "Successfully updated message. Message ID '2'.")
+                ],
+                "delete_messages": [
+                    ("1", "Invalid message ID. Please enter a valid ID."),
+                    ("2", "Invalid message ID. Please enter a valid ID."),
+                    ("3", "Invalid message ID. Please enter a valid ID."),
+                    ("100", "Invalid message ID. Please enter a valid ID."),
+                    ("1", "Failed to delete message. Message ID '100' does not exist."),
+                    ("2", "Failed to delete message. Message ID '100' does not exist."),
+                    ("3", "Failed to delete message. Message ID '100' does not exist."),
+                    ("1", "Successfully deleted message. Message ID '1'."),
+                    ("2", "Successfully deleted message. Message ID '2'.")
+                ]
+            },
+            {
+                "login_result": "Invalid username or password."
+            }
+        ]
+    }
+
+
+# Austins run update/delete function
+def run_update_delete_tests(test_set: dict):
+    print("\n{}\n".format(test_set.get("name")))
+    cases = test_set.get('cases')
+    expected = test_set.get('expected')
+
+    for i, (username, password) in enumerate(cases.items()):
+        interact_ = interact.Interact(username, password, messages.Messages("messages.txt"))
+        actual_login_result = login_test_generator(username, password)
+        print(f"Login Result: {actual_login_result}")
+
+        if actual_login_result == expected[i]["login_result"]:
+            print("Login Test: Pass\n")
+
+            for j, (update_msg_input, update_msg_expected) in enumerate(expected[i]["update_messages"]):
+                actual_update_result = interact_.update()
+                print(f"Update Message Test {j + 1}: {actual_update_result}")
+                assert actual_update_result == update_msg_expected
+
+            for j, (delete_msg_input, delete_msg_expected) in enumerate(expected[i]["delete_messages"]):
+                actual_delete_result = interact_.delete_message(int(delete_msg_input))
+                print(f"Delete Message Test {j + 1}: {actual_delete_result}")
+                assert actual_delete_result == delete_msg_expected
+
+        else:
+            print("Login Test: Fail\n")
+
+
+
+
 def login_test_generator(username, password):
     interact_ = interact.Interact(username, password, messages)
     if interact_.get_user_level() is None:
@@ -92,6 +162,9 @@ def driver_program():
         if choice == "1":
             print("\nRun Access Control sets of tests")
             run_tests(test_login())
+        elif choice == "2":
+            print("\nRun Update/Delete sets of tests")
+            run_update_delete_tests(test_update_delete_messages())
             # run_tests(test_tautology())
             # run_tests(test_union())
             # run_tests(test_additional_statement())
@@ -108,6 +181,7 @@ def driver_program():
 def print_menu():
     print("""
     1. Set of tests\n
+    2. Set of Update/Delete tests\n
     h. Display menu\n
     q. Exit\n""")
 
